4a0e2cfdf636ce39ea512cbf1e0371456da38cce
Alexander <a.tit.off@gmail.com>
2022-08-29 20:41:40 +0300

    more

--- gpio_mqtt/main.go
+++ gpio_mqtt/main.go
@@ -2,37 +2,12 @@ package main
 
 import (
 	"fmt"
-	"gopkg.in/yaml.v2"
 	"os"
 	"time"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
-type data struct {
-	Data map[string]string `yaml:"options"`
-}
-
-func loadSettings() {
-	t := data{}
-	// read settings
-	settFile, err := os.ReadFile("/data/config.yaml")
-	check(err)
-	err = yaml.Unmarshal(settFile, &t)
-	check(err)
-	if foo, ok := t.Data["MqttPort"]; ok {
-		fmt.Printf("--- t:\n%v\n\n", foo)
-	} else {
-		panic("not in config MqttPort")
-	}
-}
-
 func main() {
-	loadSettings()
+	fmt.Println("FOO:", os.Getenv("MQTT_PORT"))
 	fmt.Println(len(os.Args), os.Args)
 	for {
 		time.Sleep(10 * time.Second)

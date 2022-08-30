cccbd4e31f668762df2ec2eafb30eae771a452bd
Alexander <a.tit.off@gmail.com>
2022-08-30 08:37:03 +0300

    Update run.sh

--- gpio_mqtt/run.sh
+++ gpio_mqtt/run.sh
@@ -11,10 +11,10 @@ MQTT_PASSWORD=$(bashio::services mqtt "password")
 if [[ -r "$CONFIG_PATH" ]]
 then
   MqttPort="$(jq --raw-output '.MqttPort // empty' "$CONFIG_PATH")"
-  MqttPort="$(jq --raw-output '.MqttHost // empty' "$CONFIG_PATH")"
+  MqttHost="$(jq --raw-output '.MqttHost // empty' "$CONFIG_PATH")"
 fi
 
 export MqttPort="${MqttPort:-1883}"
 export MqttHost="${MqttHost:-localhost}"
 
-exec /gpio_mqtt --mqtthost=$MQTT_HOST --mqttuser=$MQTT_USER --mqttpass=$MQTT_PASSWORD --mqttport=1883
+exec /gpio_mqtt --mqtthost=$MQTT_HOST --mqttuser=$MQTT_USER --mqttpass=$MQTT_PASSWORD --mqttport=1883 --MqttPort=$MqttPort

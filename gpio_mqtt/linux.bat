356559e5e6e697a389196cb6feafc2ffb9aa8371
Alexander <a.tit.off@gmail.com>
2022-09-02 10:50:32 +0300

    many

--- gpio_mqtt/linux.bat
+++ gpio_mqtt/linux.bat
@@ -1,10 +1,16 @@
-set GOARCH=amd64
 rem set GOARCH=arm
 set GOOS=linux
 rem set GOOS=windows
+
 
 rem go tool dist install -v pkg/runtime
 rem go install -v -a std
 
 go build
-rem go build -o gpio_mqtt_arm
+
+
+rem set GOARCH=ARM64
+rem go build -o aarch64_gpio_mqtt
+
+set GOARCH=amd64
+go build -o amd64_gpio_mqtt
